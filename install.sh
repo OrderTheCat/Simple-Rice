9dec11a974f85824954d1ed6dcf8061ff1ae87c5
OrderTheCat <270085689+OrderTheCat@users.noreply.github.com>
2026-05-12 11:18:12 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,17 +5,20 @@ DIR1=~/All-in-one-rice/fastfetch
 DIR2=~/All-in-one-rice/kitty
 DIR3=~/All-in-one-rice/mouse-cursors
 DIR4=~/All-in-one-rice/topgrade.toml
+DIR5=~/All-in-one-rice/default
 
 # Destination directories
 DEST1=~/.config
 DEST2=~/.config
 DEST3=~/.icons
 DEST4=~/.config
+DEST5=~/.icons
 
 # Move directories
 mv "$DIR1" "$DEST1"
 mv "$DIR2" "$DEST2"
 mv "$DIR3" "$DEST3"
 mv "$DIR4" "$DEST4"
+mv "$DIR5" "$DEST5"
 
 echo "Setup done!"
